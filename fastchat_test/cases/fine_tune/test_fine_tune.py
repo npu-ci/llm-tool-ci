8e375a94c2603fc601f88e3a2bb90fd41e9e1175
jq460494839 <460494839@qq.com>
2023-12-22 11:15:33 +0800

    fix

--- fastchat_test/cases/fine_tune/test_fine_tune.py
+++ fastchat_test/cases/fine_tune/test_fine_tune.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
         'torchrun --nproc_per_node=%d --master_port=20001 fastchat/train/train.py  '
         '--model_name_or_path  %s  '
         '--data_path /opt/nlp_data/evol-instruct-chinese--1-subset.json  '
-        '--fp16 True  --output_dir output_llama  --num_train_epochs 2  '
+        '--fp16 True  --output_dir output_model  --num_train_epochs 2  '
         '--per_device_train_batch_size 8  --per_device_eval_batch_size 1  '
         '--gradient_accumulation_steps 1  --evaluation_strategy "no"  '
         '--save_strategy "steps"  --save_steps 2000  '
