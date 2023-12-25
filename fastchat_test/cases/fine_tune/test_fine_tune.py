bbfb357ce91aa95bec311a88a000e9cad92da05a
JQ <460494839@qq.com>
2023-12-25 10:48:42 +0800

    fix (#61)
    * fix
    
    * add model log
    
    * fix
    
    * check loss in scan models
    
    * 延长模型扫描执行时间

--- fastchat_test/cases/fine_tune/test_fine_tune.py
+++ fastchat_test/cases/fine_tune/test_fine_tune.py
@@ -7,7 +7,8 @@ from fastchat.utils import run_cmd
 
 def test_single_npu_fine_tune(cmd):
     model_path = "/opt/big_models/bloom-560m"
-
+    print("test_single_npu_fine_tune: model_path:%s" % model_path)
+    print("------" * 10)
     ret = run_cmd(cmd % (1, model_path))
     if ret != 0:
         raise RuntimeError("test %s in single npu error." % model_path)
@@ -21,6 +22,8 @@ def test_multi_npu_fine_tune(cmd):
         "/opt/big_models/gpt2",
     ]
     for model_path in models:
+        print("test_multi_npu_fine_tune: model_path:%s" % model_path)
+        print("------" * 10)
         if "llama" in model_path:
             _cmd = cmd + '  --fsdp_transformer_layer_cls_to_wrap "LlamaDecoderLayer"'
         else:
@@ -37,7 +40,7 @@ if __name__ == "__main__":
         'torchrun --nproc_per_node=%d --master_port=20001 fastchat/train/train.py  '
         '--model_name_or_path  %s  '
         '--data_path /opt/nlp_data/evol-instruct-chinese--1-subset.json  '
-        '--fp16 True  --output_dir output_llama  --num_train_epochs 2  '
+        '--fp16 True  --output_dir output_model  --num_train_epochs 2  '
         '--per_device_train_batch_size 8  --per_device_eval_batch_size 1  '
         '--gradient_accumulation_steps 1  --evaluation_strategy "no"  '
         '--save_strategy "steps"  --save_steps 2000  '
