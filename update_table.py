514ac1c97d89a8e08a58f88e468409f476307584
wangshuai09 <391746016@qq.com>
2024-01-22 15:32:39 +0800

    update accelerate

--- update_table.py
+++ update_table.py
@@ -5,23 +5,35 @@ import argparse
 PATTERN = r"<!--{}-{}-->"
 
 
-def update_table(file_path, table_path, tool_name):
+def update_table(file_path, table_path, table_item, tool_name):
     with open(file_path, "r") as f:
         text = f.read()
 
-    with open(table_path, "r") as f:
-        table = json.load(f)
+    if table_path:
+        with open(table_path, "r") as f:
+            table = json.load(f)
 
-    for model, support in table.items():
+        for model, support in table.items():
+            pattern = PATTERN.format(tool_name, model)
+            match = re.search(pattern, text, re.IGNORECASE)
+            if match:
+                past_state = text[match.start() : match.end() + 1]
+                current_state = past_state[:-1] + ("✅" if support else " ")
+                text = text.replace(past_state, current_state)
+
+        with open(file_path, "w") as f:
+            f.write(text)
+    elif table_item:
+        model, support = table_item.split(":")
         pattern = PATTERN.format(tool_name, model)
         match = re.search(pattern, text, re.IGNORECASE)
         if match:
             past_state = text[match.start() : match.end() + 1]
-            current_state = past_state[:-1] + ("✅" if support else " ")
+            current_state = past_state[:-1] + ("✅" if int(support)==0 else " ")
             text = text.replace(past_state, current_state)
+        with open(file_path, "w") as f:
+            f.write(text)
 
-    with open(file_path, "w") as f:
-        f.write(text)
 
 
 parser = argparse.ArgumentParser()
@@ -39,20 +51,35 @@ parser.add_argument(
     choices=["transformers", "accelerate", "peft", "fschat"],
     help="Specify a tool name.",
 )
+parser.add_argument(
+    "--table-item", "-ti", type=str, default=None, help="Specify a table item key:value str."
+)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
     file_path = args.file_path
     table_path = args.table_path
     tool_name = args.tool_name
+    table_item = args.table_item
 
-    if (type(file_path) is not str) or (type(table_path) is not str):
-        raise ValueError("file-path must be a file path of str type.")
+    if not table_item and not table_path:
+        raise ValueError("Must configure one of table_item or table_path.")
+    
+    if table_item and (type(table_item) is not str):
+        raise ValueError("table_item must be a key:value str.")
+    
+    if file_path:
+        if type(file_path) is not str:
+            raise ValueError("file-path or table_path must be a file path of str type.")
+        if not file_path.endswith(".md"):
+            raise ValueError("file-path must be a Markdown file.")
+    
+    if table_path:
+        if type(table_path) is not str:
+            raise ValueError("file-path or table_path must be a file path of str type.")
+        if not table_path.endswith(".json"):
+          raise ValueError("file-path must be a Json file.")
 
-    if not file_path.endswith(".md"):
-        raise ValueError("file-path must be a Markdown file.")
 
-    if not table_path.endswith(".json"):
-        raise ValueError("file-path must be a Json file.")
-
-    update_table(file_path, table_path, tool_name)
+    update_table(file_path, table_path, table_item, tool_name)
